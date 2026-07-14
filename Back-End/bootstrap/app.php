332882b9f171998fbb1937073887b464c6465bcc
Ahmad Yusuf Ar-Rafi <yusufzolldyck@gmail.com>
2026-07-14 20:38:57 +0700

    Enable web and console routes in app configuration

--- Back-End/bootstrap/app.php
+++ Back-End/bootstrap/app.php
@@ -9,8 +9,8 @@ use Spatie\Permission\Middleware\RoleMiddleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__ . '/../routes/api.php',
-        // web: __DIR__.'/../routes/web.php',
-        // commands: __DIR__.'/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
